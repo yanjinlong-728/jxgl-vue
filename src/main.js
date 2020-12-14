c325832e7cdbc5cbb0eef2c50a427328854a4c78
yanjinlong-728 <yanjinlong728@163.com>
2020-12-14 11:07:04 +0800

    增加了element-ui

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import 'lib-flexible/flexible'
 
 Vue.use(Mint)  
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
